d5b26854ba89b8cfbcf5d0c02d620fb41c5c5fb7
Milkitic <Milkitic@126.com>
2022-10-20 14:03:27 +0800

    Fix designer

--- iBMSC/SplashScreen1.Designer.cs
+++ iBMSC/SplashScreen1.Designer.cs
@@ -27,19 +27,23 @@ namespace iBMSC
     [System.Diagnostics.DebuggerStepThrough]
     private void InitializeComponent()
     {
-        this.SuspendLayout();
-this.AutoScaleDimensions=new System.Drawing.SizeF(6f, 13f);
-        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-this.ClientSize=new System.Drawing.Size(722, 331);
-        this.ControlBox = false;
-        this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-        this.MaximizeBox = false;
-        this.MinimizeBox = false;
-        this.Name = "SplashScreen1";
-        this.ShowInTaskbar = false;
-        this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-        this.ResumeLayout(false);
+            this.SuspendLayout();
+            // 
+            // SplashScreen1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(722, 306);
+            this.ControlBox = false;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "SplashScreen1";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.ResumeLayout(false);
+
     }
 
     }
